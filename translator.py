150d338d3698ca13ab757e098ac9aef76294b11a
spaceowlsoul <yuliasunny007@gmail.com>
2022-10-09 15:44:21 +0300

    Revert "stage 6/7"
    This reverts commit 3ccb005692667aa7b8d8cdcb301ff63465bf42f0.

--- translator.py
+++ translator.py
@@ -1,4 +1,3 @@
-import sys
 import requests
 from bs4 import BeautifulSoup
 
@@ -6,8 +5,9 @@ url = 'https://context.reverso.net/translation/'
 user_agent = 'Mozilla/5.0'
 headers = {'User-Agent': user_agent}
 
-lang_list = ['arabic', 'german', 'english', 'spanish', 'french', 'hebrew', 'japanese',
-             'dutch', 'polish', 'portuguese', 'romanian', 'russian', 'turkish']
+lang_dict = {'1': 'arabic', '2': 'german', '3': 'english', '4': 'spanish', '5': 'french',
+             '6': 'hebrew', '7': 'japanese', '8': 'dutch', '9': 'polish', '10': 'portuguese',
+             '11': 'romanian', '12': 'russian', '13': 'turkish'}
 
 
 def output(language_2, translations, examples, output_num):
@@ -35,59 +35,48 @@ def parsing(trans_page):
     trans_tags = soup.find_all('span', {'class': 'display-term'})
     examples_tags = soup.find('section', id="examples-content").find_all('span', class_="text")
 
-    if trans_tags and examples_tags:
-        translations = [t.text for t in trans_tags]
-        examples = [e.text.strip() for e in examples_tags]
-        i = 2
-        while i < len(examples):
-            examples.insert(i, ' ')
-            i += 3
-        return translations, examples
+    translations = [t.text for t in trans_tags]
+    examples = [e.text.strip() for e in examples_tags]
+    i = 2
+    while i < len(examples):
+        examples.insert(i, ' ')
+        i += 3
+    return translations, examples
 
 
 def translation(from_, to_, word):
-    language_1 = from_
+    language_1 = lang_dict[from_]
     languages_2 = []
-    if to_ != 'all':
-        languages_2.append(to_)
+    if to_ != '0':
+        languages_2.append(lang_dict[to_])
     else:
-        for lang in lang_list:
-            if lang != from_:
-                languages_2.append(lang)
+        for num in lang_dict.keys():
+            if num != from_:
+                languages_2.append(lang_dict[num])
     for language_2 in languages_2:
         trans_page = f'{url}{language_1}-{language_2}/{word}'
-        if parsing(trans_page):
-            translations, examples = parsing(trans_page)
+        translations, examples = parsing(trans_page)
 
-            output_num = 1
+        output_num = 1
 
-            output(language_2, translations, examples, output_num)
-            export_to_file(word, language_2, translations, examples, output_num)
-        else:
-            print(f'Sorry, unable to find {word}')
-            break
-
-
-def check_input(from_, to_):
-    if from_ not in lang_list:
-        print(f"Sorry, the program doesn't support {from_}")
-        return False
-    elif to_ not in lang_list and to_ != 'all':
-        print(f"Sorry, the program doesn't support {to_}.")
-        return False
-    return True
+        output(language_2, translations, examples, output_num)
+        export_to_file(word, language_2, translations, examples, output_num)
 
 
 def main():
-    from_, to_ = sys.argv[1].lower(), sys.argv[2].lower()
-    word = sys.argv[3]
+    print('Hello, welcome to the translator. Translator supports:')
+    for num, lang in lang_dict.items():
+        print('{}. {}'.format(num, lang.title()))
 
-    if check_input(from_, to_):
-        r = requests.get(url, headers=headers)
-        if r.status_code == 200:
-            translation(from_, to_, word)
-        else:
-            print('Something wrong with your internet connection')
+    from_ = input('Type the number of your language:\n')
+    to_ = input('''Type the number of a language you want to translate to
+or '0' to translate to all languages:\n''')
+    word = input('Type the word you want to translate:\n')
+
+    r = requests.get(url, headers=headers)
+    if r.status_code == 200:
+        print(r.status_code, 'OK\n')
+        translation(from_, to_, word)
 
 
 if __name__ == "__main__":
