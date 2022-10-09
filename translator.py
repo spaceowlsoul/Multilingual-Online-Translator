c4a8e7b2130d1a83ff35cbabfb55efad8af66e05
spaceowlsoul <yuliasunny007@gmail.com>
2022-10-09 15:49:15 +0300

    stage 6/7

--- translator.py
+++ translator.py
@@ -35,14 +35,13 @@ def parsing(trans_page):
     trans_tags = soup.find_all('span', {'class': 'display-term'})
     examples_tags = soup.find('section', id="examples-content").find_all('span', class_="text")
 
-    if trans_tags and examples_tags:
-        translations = [t.text for t in trans_tags]
-        examples = [e.text.strip() for e in examples_tags]
-        i = 2
-        while i < len(examples):
-            examples.insert(i, ' ')
-            i += 3
-        return translations, examples
+    translations = [t.text for t in trans_tags]
+    examples = [e.text.strip() for e in examples_tags]
+    i = 2
+    while i < len(examples):
+        examples.insert(i, ' ')
+        i += 3
+    return translations, examples
 
 
 def translation(from_, to_, word):
@@ -56,39 +55,24 @@ def translation(from_, to_, word):
                 languages_2.append(lang)
     for language_2 in languages_2:
         trans_page = f'{url}{language_1}-{language_2}/{word}'
-        if parsing(trans_page):
-            translations, examples = parsing(trans_page)
+        translations, examples = parsing(trans_page)
 
-            output_num = 1
+        output_num = 1
 
-            output(language_2, translations, examples, output_num)
-            export_to_file(word, language_2, translations, examples, output_num)
-        else:
-            print(f'Sorry, unable to find {word}')
-            break
-
-
-def check_input(from_, to_):
-    if from_ not in lang_list:
-        print(f"Sorry, the program doesn't support {from_}")
-        return False
-    elif to_ not in lang_list and to_ != 'all':
-        print(f"Sorry, the program doesn't support {to_}.")
-        return False
-    return True
+        output(language_2, translations, examples, output_num)
+        export_to_file(word, language_2, translations, examples, output_num)
 
 
 def main():
-    from_, to_ = sys.argv[1].lower(), sys.argv[2].lower()
+    from_ = sys.argv[1].lower()
+    to_ = sys.argv[2].lower()
     word = sys.argv[3]
 
-    if check_input(from_, to_):
-        r = requests.get(url, headers=headers)
-        if r.status_code == 200:
-            translation(from_, to_, word)
-        else:
-            print('Something wrong with your internet connection')
+    r = requests.get(url, headers=headers)
+    if r.status_code == 200:
+        translation(from_, to_, word)
 
 
 if __name__ == "__main__":
     main()
+
